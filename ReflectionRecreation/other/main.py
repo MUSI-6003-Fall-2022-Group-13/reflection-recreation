17e6bddcb3884d04f8177f87facd8a016e8c436d
Karl Hiner <karl.hiner@gmail.com>
2022-12-12 14:24:02 -0500

    Minor cleanup in main

--- ReflectionRecreation/other/main.py
+++ ReflectionRecreation/other/main.py
@@ -15,8 +15,8 @@ class ReflectionRecreation():
         self.MIDI_FILE_NAMES=[i for i in range(1,10+1)]
         self.FILE_SELECTION_PROBS = [1/len(self.MIDI_FILE_NAMES) for i in range(len(self.MIDI_FILE_NAMES))]
         self.current_midi_note = 0
-        
-    
+
+
     def play(self):
         while(True):
             try:
@@ -25,26 +25,27 @@ class ReflectionRecreation():
                 print(f"Using file: {midi_file_selected}.mid")
                 midi_data = mido.MidiFile(os.path.join(os.path.dirname(__file__),'..','resources',f'{midi_file_selected}.mid'))
                 for msg in midi_data:
-                    if not msg.is_meta:
-                        if msg.type == 'note_off':
-                            time.sleep(msg.time)
-                            print(f"Note duration from file: {msg.time} seconds")
-                            self.send_note(self.current_midi_note, 0)
-                            continue
-                        sleep_dur = random.uniform(0.1, 3)
-                        print(f"Adding a rest for {sleep_dur} seconds")
-                        time.sleep(sleep_dur)
-                        self.current_midi_note = self.process_note(msg.note)
-                        self.send_note(self.current_midi_note, 1)
+                    if msg.is_meta:
+                        continue
+                    if msg.type == 'note_off':
+                        time.sleep(msg.time)
+                        print(f"Note duration from file: {msg.time} seconds")
+                        self.send_note(self.current_midi_note, 0)
+                        continue
+                    sleep_dur = random.uniform(0.1, 3)
+                    print(f"Adding a rest for {sleep_dur} seconds")
+                    time.sleep(sleep_dur)
+                    self.current_midi_note = self.process_note(msg.note)
+                    self.send_note(self.current_midi_note, 1)
             except KeyboardInterrupt:
                 print("Keyboard interrupt")
                 break
 
 
-    
+
     def send_note(self, midi_note, note_type):
-        self.client.send_message("type", note_type)
-        self.client.send_message("note", midi_note)
+        self.client.send_message('type', note_type)
+        self.client.send_message('note', midi_note)
         print(f'Sending note: {midi_note} and type: {note_type}')
         return
 
@@ -58,13 +59,9 @@ class ReflectionRecreation():
         else:
             return midi_note
 
-    
     def shift_third(self, midi_note, mode='major'):
-        if mode == 'major':
-            return midi_note-4
-        else:
-            return midi_note-3
-    
+        return -4 if mode == 'major' else -3
+
     def shift_octave(self, midi_note):
         return midi_note-12
 
